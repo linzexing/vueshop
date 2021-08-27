d74ba9913ae5bb1753206e258969c86d51a9563b
linzexing <692948952@qq.com>
2021-08-28 02:56:24 +0800

    登录成功，保存token值

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -31,6 +31,10 @@ export default {
     };
   },
   methods: {
+    /*让异步代码ajax看起来像同步代码
+    1、找到异步操作有结果的代码，见面夹await，同时接收异步操作的结果res
+    2、找到距离异步操作有结果的代码最近的方法，前面加async
+    */
     //登录请求
     async handleLogin() {
       const res = await this.$http.post("login", this.formdata);
@@ -38,8 +42,10 @@ export default {
         data,
         meta: { msg, status },
       } = res.data;
-
+      //登录成功
       if (status === 200) {
+        //保存token
+        localStorage.setItem('token',data.token)
         //跳转到home页面
         this.$router.push({ name: "home" });
         //提示成功信息
